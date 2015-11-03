a298d954193fe22070b273f78abb955709a2c67f
tattyamm <entatsu.appli+github@gmail.com>
2015-11-03 21:44:06 +0900

    add error handling

--- app/src/main/java/jp/tattyamm/android/checktrend/MainActivityFragment.java
+++ app/src/main/java/jp/tattyamm/android/checktrend/MainActivityFragment.java
@@ -1,5 +1,6 @@
 package jp.tattyamm.android.checktrend;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v4.app.Fragment;
@@ -20,6 +21,7 @@ import android.widget.Toast;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
@@ -28,6 +30,8 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+
 import android.widget.AdapterView.OnItemClickListener;
 
 /**
@@ -54,45 +58,59 @@ public class MainActivityFragment extends Fragment {
 
         Button button01 = (Button)getActivity().findViewById(R.id.selectButton01);
         Button button02 = (Button)getActivity().findViewById(R.id.selectButton02);
+        Button button03 = (Button)getActivity().findViewById(R.id.selectButton03);
+        Button button04 = (Button)getActivity().findViewById(R.id.selectButton04);
 
         button01.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                reloadView(getActivity(), "1 url");
+                reloadView(getActivity(), "Googleトレンド", "http://checktrend.herokuapp.com/api/trend/google.json");
             }
         });
-
         button02.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                reloadView(getActivity(), "2 url");
+                reloadView(getActivity(), "Yahoo急上昇", "http://checktrend.herokuapp.com/api/trend/yahoo.json");
+            }
+        });
+        button03.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                reloadView(getActivity(), "twitterトレンド", "http://checktrend.herokuapp.com/api/trend/twitter.json");
+            }
+        });
+        button04.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                reloadView(getActivity(), "Amazonランキング", "http://checktrend.herokuapp.com/api/trend/amazon.json");
             }
         });
     }
 
-    public void reloadView(FragmentActivity fActivity, String url) {
+    public void reloadView(FragmentActivity fActivity, String viewTitle, String url) {
         TextView textView = (TextView)fActivity.findViewById(R.id.textview);
-        textView.setText(url);
-
-        Toast.makeText(fActivity, url, Toast.LENGTH_SHORT).show();
-
-        requestJson(fActivity);
+        textView.setText(viewTitle);
+        requestJson(fActivity, url);
     }
 
     //http://dev.classmethod.jp/smartphone/android/android-tips-51-volley/
-    private void requestJson(FragmentActivity fActivity) {
+    private void requestJson(FragmentActivity fActivity, String url) {
         final TextView textView = (TextView)fActivity.findViewById(R.id.textview);
         final ListView listView = (ListView) fActivity.findViewById(R.id.listview);
 
+        final ProgressDialog pDialog = new ProgressDialog(fActivity);
+        pDialog.setMessage("Loading...");
+        pDialog.show();
+
         // Volley でリクエスト
-        //TODO URLを分岐
-        String url = "http://checktrend.herokuapp.com/api/trend/google.json";
         mQueue.add(new JsonObjectRequest(Request.Method.GET, url, null,
                 new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
                         //Log.d(TAG, "response : " + response.toString());
                         //textView.setText(response.toString());
+
+                        pDialog.hide();
 
                         //jsonにして、テーブル表示
                         String temp = "";
@@ -109,7 +127,9 @@ public class MainActivityFragment extends Fragment {
                                 listLink.add(link);
                             }
                         } catch (JSONException e) {
-                            listTitle.add("結果が取得できませんでした");
+                            //エラー処理
+                            listTitle.add("読み込みエラーが発生しました");
+                            listLink.add("https://www.google.co.jp/trends/");
                         }
 
                         //listに表示
@@ -127,7 +147,22 @@ public class MainActivityFragment extends Fragment {
                             }
                         });
                     }
-                }, null));
+                },
+                new Response.ErrorListener(){
+                    @Override
+                    public void onErrorResponse(VolleyError error){
+                        pDialog.hide();
+
+                        //listに表示
+                        String errorMsg = "通信に失敗しました。時間を置いて再度接続してください。";
+                        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(
+                                getActivity(), android.R.layout.simple_list_item_1, Arrays.asList(errorMsg)
+                        );
+                        listView.setAdapter(arrayAdapter);
+
+                    }
+                }
+        ));
         mQueue.start();
     }
 
